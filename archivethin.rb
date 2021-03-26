923767b9c30b60a678d5303d4fd6d61cd2c82f09
Noah Martin <noahm444@gmail.com>
2021-03-25 17:10:13 -0700

    Update version

--- archivethin.rb
+++ archivethin.rb
@@ -2,12 +2,12 @@ class Archivethin < Formula
   desc "Thin 32bit architecture from iOS archives"
   homepage "https://github.com/EmergeTools/ArchiveThin"
   url "https://github.com/EmergeTools/ArchiveThin.git",
-      :tag => "0.0.2"
+      :tag => "0.0.3"
   head "https://github.com/EmergeTools/ArchiveThin.git"
 
   depends_on :xcode => ["12.0", :build]
 
   def install
-    system "make", "install"
+    system "make", "install", "PREFIX=#{prefix}"
   end
 end
