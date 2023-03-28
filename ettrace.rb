c24815dff314715c17a39ea7d89482d490757e5b
Noah Martin <noahm444@gmail.com>
2023-03-28 10:43:46 -0400

    Add ETTrace

---
+++ ettrace.rb
@@ -0,0 +1,12 @@
+class Ettrace < Formula
+  desc "Easily and accurately profile iOS apps"
+  homepage "https://github.com/EmergeTools/ETTrace"
+  url "https://github.com/EmergeTools/ETTrace/releases/download/v0.1/ETTraceRunner"
+  sha256 "824f989714b17430486d527cd1dc970e78f46bac33e54d7782298822f70a6bd4"
+  license "MIT"
+
+
+  def install
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+  end
+end
