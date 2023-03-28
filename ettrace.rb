58a7fe8156b7b637b0197196648ccaf7daa79dcf
Noah Martin <noahm444@gmail.com>
2023-03-28 11:04:54 -0400

    Update sha

--- ettrace.rb
+++ ettrace.rb
@@ -2,7 +2,7 @@ class Ettrace < Formula
   desc "Easily and accurately profile iOS apps"
   homepage "https://github.com/EmergeTools/ETTrace"
   url "https://github.com/EmergeTools/ETTrace/releases/download/v0.1/ETTraceRunner"
-  sha256 "824f989714b17430486d527cd1dc970e78f46bac33e54d7782298822f70a6bd4"
+  sha256 "aa916eca00ad1431fb0ca15b5c7edf34c664072bf36d8d0491e52a7c434f77a0"
   license "MIT"
 
   def install
